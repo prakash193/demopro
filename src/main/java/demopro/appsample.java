0170ddc93cc8e66d48c6b55cbfcaba2c34dba433
prakash193 <kilari.prakash193@gmail.com>
2018-09-07 12:11:24 +0530

    demo project

---
+++ src/main/java/demopro/appsample.java
@@ -0,0 +1,10 @@
+package demopro;
+
+public class appsample {
+	 public static void main(String[] args) {
+	        System.out.println("Hello World");
+	    }
+		
+	}
+
+
---
+++ src/test/java/demopro/demotest.java
@@ -0,0 +1,14 @@
+package demopro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class demotest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
